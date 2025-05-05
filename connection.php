06c1226bd69b320c6d388314bc4e2d2e7d80254e
rendybustari <rendybustari@gmail.com>
2025-05-05 07:59:21 +0700

    fix: update database connection settings for local development

--- connection.php
+++ connection.php
@@ -1,13 +1,12 @@
 <?php
-//$servername = "mysql_db_secure_coding"; // Ganti dengan server database Anda jika perlu
-$servername = "localhost"; // Ganti dengan server database Anda jika perlu
+$servername = "mysql_db_secure_coding"; // Ganti dengan server database Anda jika perlu
 $username = "root"; // Ganti dengan username database Anda
 $password = "root"; // Ganti dengan password database Anda
-$dbname = "secure_coding"; // Nama database yang dibuat sebelumnya
+$dbname = "db_secure_coding"; // Nama database yang dibuat sebelumnya
 $port = 3306;
 // Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
-$host  = 'http://localhost:8000';
+$host  = 'http://127.0.0.1:8003';
 // Memeriksa koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
